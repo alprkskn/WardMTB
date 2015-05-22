12212a608a4f4f92a4dd9926de7774b396bc09dd
Alper Keskin <alprkskn@gmail.com>
2015-05-22 20:04:19 +0300

    Implemented MTB. Project additionally requires bitarray library from now on.
    Created an IMG class under hdrutils.py
    It initializes MTB bitarray under self.bits

--- src/WardMTB.py
+++ src/WardMTB.py
@@ -1,13 +1,17 @@
 import hdrutils as hd
 
-def get_luminances(img):
-    result = []
-    for row in img:
-        r = []
-        for pixel in row:
-            r.append((pixel[0]*54 + pixel[1]*183 + pixel[2]*19)/256)
-        result.append(r)
-    return result
 
 images = hd.get_images(hd.path_set_1, False)
 
+i0 = hd.IMG(images[0])
+#i1 = hd.IMG(images[1])
+#i2 = hd.IMG(images[2])
+
+
+#i0.save_grayscale("img/lol_0.png")
+#i1.save_grayscale("img/lol_1.png")
+#i2.save_grayscale("img/lol_2.png")
+
+i0.save_mtb("img/mtb_0.png")
+#i1.save_mtb("img/mtb_1.png")
+#i2.save_mtb("img/mtb_2.png")
--- src/hdrutils.py
+++ src/hdrutils.py
@@ -1,8 +1,8 @@
 import os 
 from PIL import Image
-from numpy import array, uint8
+from numpy import array, uint8, median
 from math import ceil
-
+from bitarray import bitarray
 
 OVER_SATURATION_LIMIT = 245
 UNDER_SATURATION_LIMIT = 10
@@ -10,8 +10,8 @@ MAX_SIZE = 1024
 N = 4
 SAMPLE_COUNT = 500
 
-path_set_1 = "/home/bs07/e1746163/HDR_Toolbox/demos/stack"
-path_set_2 = "/home/bs07/e1746163/HDR_Toolbox/demos/stack_alignment"
+path_set_1 = "C:\HDR_Toolbox\demos\stack"
+path_set_2 = "C:\HDR_Toolbox\demos\stack_alignment"
 
 def get_images(directory, resize):
     images = []
@@ -27,3 +27,61 @@ def get_images(directory, resize):
         arr = array(img)
         images.append(arr)
     return images
+
+def get_luminances(img):
+    result = []
+    for row in img:
+        r = []
+        for pixel in row:
+            r.append((pixel[0]*54 + pixel[1]*183 + pixel[2]*19)/256)
+        result.append(r)
+    return result
+
+def image_array_from_luminances(lum):
+    result = []
+    for row in lum:
+        r = []
+        for pixel in row:
+            r.append([pixel, pixel, pixel])
+        result.append(r)
+    return array(result, "uint8")
+
+def flatten_luminance_array(arr):
+    luminances = []
+    for row in arr:
+        for pixel in row:
+            luminances.append(pixel)
+    return luminances
+
+
+class IMG:
+    def __init__(self, image):
+        self.height = len(image)
+        self.width = len(image[0])
+        self.image = image
+        self.formatted_lum = get_luminances(image)
+        self.luminances = flatten_luminance_array(self.formatted_lum)
+        self.bits = bitarray()
+        self.compute_mtb()
+        print "Init image", median(self.luminances)
+
+    def save_mtb(self, path):
+        bytes = self.bits.to01()
+        arr = []
+        for x in range(self.height):
+            row = []
+            for y in range(self.width):
+                pixel = bytes[x*self.width+y]
+                row.append([pixel, pixel, pixel])
+            arr.append(row)
+        img = Image.fromarray(array(arr, "uint8") * 255)
+        img.save(path, path.split('.')[-1])            
+
+    def save_grayscale(self, path):
+        img = Image.fromarray(image_array_from_luminances(self.formatted_lum))
+        img.save(path, path.split('.')[-1])
+
+    def compute_mtb(self):
+        m = median(self.luminances)
+        for p in self.luminances:
+            self.bits.append(p > m)
